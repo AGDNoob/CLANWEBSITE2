1b17739bb96990b0de83bba4d1f0b1822c6eb2e4
AGDNoob <weifenmarlon250@gmail.com>
2025-08-28 17:31:00 +0200

    Update script.js

--- frontend/script.js
+++ frontend/script.js
@@ -88,7 +88,7 @@ document.addEventListener('DOMContentLoaded', () => {
     // GLOBALE VARIABLEN & HILFSFUNKTIONEN
     // ======================================================
     // WICHTIG: Ersetze DEIN-USERNAME durch deinen echten PythonAnywhere Benutzernamen
-    const API_BASE_URL = 'https://magicdragons.vercel.app';
+    const API_BASE_URL = 'https://clanwebsite-2.vercel.app';
     let currentMemberList = [];
     let currentWarData = null;
     let cwlAllRoundsData = [];
@@ -613,3 +613,4 @@ document.addEventListener('DOMContentLoaded', () => {
     fetchCWLGroup();
 
 });
+
