8d4f326831d2d487ca56c92bedac91dfbb443369
AGDNoob <weifenmarlon250@gmail.com>
2025-09-02 23:06:44 +0200

    Update wsgi.py

--- backend/wsgi.py
+++ backend/wsgi.py
@@ -1,9 +1,10 @@
 import sys
 
 # Pfad zu deinem Backend-Ordner
-path = '/home/DEINUSERNAME/CLANWEBSITE2/backend'
+path = '/home/AGDNoob1/CLANWEBSITE2/backend'
 if path not in sys.path:
     sys.path.insert(0, path)
 
 # Importiere deine (jetzt temporäre) app
 from app import app as application
+
